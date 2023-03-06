468749374dee8f8681d12e5c654cb35ec38db402
Clare Morris <morrisclarehart@gmail.com>
2023-03-06 17:00:34 -0500

    Created controller folder.

--- election/src/main/java/com/cityhall/election/controllers/BikeController.java
+++ election/src/main/java/com/cityhall/election/controllers/BikeController.java
@@ -1,4 +1,4 @@
-package com.cityhall.election;
+package com.cityhall.election.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
