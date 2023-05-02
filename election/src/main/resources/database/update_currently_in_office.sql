e903c8ac4ec2974ab4a31c2ff30de1a7a0bc48ed
Clare Morris <morrisclarehart@gmail.com>
2023-05-02 17:33:52 -0400

    Made it so that electionId can be inserted in proc

--- election/src/main/resources/database/update_currently_in_office.sql
+++ election/src/main/resources/database/update_currently_in_office.sql
@@ -1,6 +1,6 @@
-CREATE OR REPLACE PROCEDURE update_currently_in_office AS
+create or replace PROCEDURE update_currently_in_office(electionId in  integer) AS
 BEGIN
   UPDATE PREVIOUSLY_ELECTED
   SET currently_in_office = 'N'
-  WHERE election_id=2;
+  WHERE election_id=electionId;
 END update_currently_in_office;
